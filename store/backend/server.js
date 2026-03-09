cb3f878936d7d3b551775e8c8a5b2e77f2c1b7ff
pratikshit19 <pratikshit@icloud.com>
2026-03-09 18:51:00 +0530

    named path parsing

--- store/backend/server.js
+++ store/backend/server.js
@@ -11,7 +11,7 @@ app.use("/api/payment", paymentRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./frontend/build")));
   
-  app.get("/:any*", (req, res) => {
+  app.get("/:path*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
   });
 }
