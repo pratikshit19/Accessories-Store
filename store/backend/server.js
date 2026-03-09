5ab18d87cfb55dd9ef509e293babeb294c56e657
pratikshit19 <pratikshit@icloud.com>
2026-03-09 18:11:19 +0530

    Corrected server.js

--- store/backend/server.js
+++ store/backend/server.js
@@ -8,10 +8,10 @@ app.use("/api/payment", paymentRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
   
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
   });
 }
 connectDB();
